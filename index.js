10c9f78d15cd90e945f9a5e95ab0cd9a789ba242
Josh Gillies <github@joshgilli.es>
2015-09-11 09:34:02 +1000

    Add support for recursively adding files and folders to a bundle

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function help () {
     '  --version, -v        show version information',
     '  --entry, -e          the entry point for bundler, default "."',
     '  --output, -o         the exported file bundle, default "./bundle.tgz"',
+    '  --recursive, -r      recursively include subdirectories, default "false"',
     '  --parent, -p         an asset id to import the files under, default "1"',
     '  --link, -l           the link type all assets will be imported as, default "TYPE_1"',
     '  --unrestricted, -u   whether unrestricted access is allowed, default "false"',
@@ -29,32 +30,9 @@ function help () {
 function cli (opts) {
   var readdirp = require('readdirp')
   var Bundler = require('node-matrix-bundler')
-  var writer = require('node-matrix-importer')()
+  var writer = require('node-matrix-importer')({ sorted: true })
   var path = require('path')
   var fs = require('fs')
-
-  if (!opts.rootNode) {
-    opts.rootNode = writer.createAsset({
-      type: 'folder'
-    }).id
-
-    writer.setAttribute({
-      assetId: opts.rootNode,
-      attribute: 'name',
-      value: path.basename(path.resolve(__dirname, opts.entry))
-    })
-
-    writer.setAttribute({
-      assetId: opts.rootNode,
-      attribute: 'short_name',
-      value: path.basename(path.resolve(__dirname, opts.entry))
-    })
-
-    writer.addPath({
-      assetId: opts.rootNode,
-      path: path.basename(path.resolve(__dirname, opts.entry))
-    })
-  }
 
   var bundle = Bundler({
     writer: writer,
@@ -62,19 +40,58 @@ function cli (opts) {
     globalRootNode: opts.rootNode,
     globalUnrestricted: opts.unrestricted
   })
-  var files = readdirp({
-    root: opts.entry,
-    fileFilter: ['!.*'] // ignore hidden files
+  var entries = readdirp({
+    root: path.resolve(__dirname, opts.entry),
+    fileFilter: ['!.*'], // ignore hidden files
+    directoryFilter: opts.recursive ? ['!.*'] : ['!.*', '!*'], // ignore hidden folders
+    entryType: 'both'
   })
+  var folders = {}
 
-  files
-    .on('data', function processFile (file) {
-      bundle.add(file.fullPath)
+  entries
+    .on('data', function processFile (entry) {
+      var fullPath = entry.fullPath
+      var fullParentDir = entry.fullParentDir
+      var opts = {}
+
+      if (!~Object.keys(folders).indexOf(fullParentDir)) {
+        folders[fullParentDir] = createFolder(
+          path.basename(fullParentDir),
+          folders[path.resolve(fullParentDir, '../')] || bundle.globalRootNode
+        ).id
+      }
+
+      opts['parentId'] = folders[fullParentDir]
+
+      if (entry.stat.isFile()) {
+        bundle.add(fullPath, opts)
+      }
     })
     .on('end', function createBundle () {
       bundle.createBundle()
         .pipe(fs.createWriteStream(opts.output))
     })
+
+  function createFolder (name, parentId) {
+    var folder = writer.createAsset({
+      parentId: parentId,
+      type: 'folder'
+    })
+    var folderId = folder.id
+
+    writer.setAttribute({
+      assetId: folderId,
+      attribute: 'name',
+      value: name
+    })
+
+    writer.addPath({
+      assetId: folderId,
+      path: name
+    })
+
+    return folder
+  }
 }
 
 if (argv.version || argv.v) {
@@ -85,6 +102,7 @@ if (argv.version || argv.v) {
   cli({
     entry: argv.entry || argv.e || argv._[0] || './',
     output: argv.output || argv.o || argv._[1] || './bundle.tgz',
+    recursive: argv.recursive || argv.r,
     rootNode: argv.parent || argv.p,
     linkType: argv.link || argv.l,
     unrestricted: argv.unrestricted || argv.u
